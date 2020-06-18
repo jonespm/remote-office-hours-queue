c3ed372178653242fadc7203a4254eff84ab02fc
James Ostrander <jlost@umich.edu>
2020-06-18 15:17:01 -0400

    Add EMAIL_SUBJECT_PREFIX

--- src/officehours/settings.py
+++ src/officehours/settings.py
@@ -281,6 +281,7 @@ LOGGING_METHODS = csv_to_list(
 
 # Email
 EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_SUBJECT_PREFIX = os.getenv('EMAIL_SUBJECT_PREFIX', 'ROHQ').strip()
 
 ADMINS = [('Admins', os.getenv('ADMIN_EMAIL'))]
 MANAGERS = ADMINS
